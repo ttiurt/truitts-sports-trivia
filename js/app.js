8a59c42fb50699b79aa27e9b6e19afe43c1564e5
ttiurt <truittzach33@gmail.com>
2023-04-01 20:28:03 -0400

    def variables

--- js/app.js
+++ js/app.js
@@ -3,7 +3,14 @@
 
 
 /*---------------------------- Variables (state) ----------------------------*/
-
+const quiz = document.getElementById("quiz")
+const qImage = document.getElementById("image")
+const question = document.getElementById("question")
+const choices = document.getElementById("choices")
+const choiceA = document.getElementById("A")
+const choiceB = document.getElementById("B")
+const choiceC = document.getElementById("C")
+const choiceD = document.getElementById("D")
 
 
 /*------------------------ Cached Element References ------------------------*/
